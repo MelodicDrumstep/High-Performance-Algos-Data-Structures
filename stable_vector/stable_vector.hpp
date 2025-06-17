1f30bc91af299495f02df404d24819b1cd751ee1
MelodicDrumstep <1724662527@qq.com>
2025-06-17 14:19:44 +0800

    Implement iterator and other member functions for stable vector and pass the correctness test.

--- stable_vector/stable_vector.hpp
+++ stable_vector/stable_vector.hpp
@@ -10,11 +10,11 @@
 #include <concepts>
 #include <type_traits>
 
-#define DEBUG_STABLE_VECTOR
+// #define DEBUG_STABLE_VECTOR
 
 namespace hpds {
 
-template <typename T, std::size_t ChunkSize = 1024, std::size_t InitialCapacity = 1024>
+template <typename T, std::size_t ChunkSize = 256, std::size_t InitialCapacity = 256>
 requires ((((ChunkSize) & (ChunkSize - 1)) == 0))
 // make sure ChunkSize is a power of 2, for the compiler to optimize
 // out modular operations
@@ -43,10 +43,9 @@ class StableVector {
             if(chunk_size >= ChunkSize) {
                 throw std::out_of_range("[Chunk::push_back]");
             }
-            new (elements + chunk_size) T(std::forward<Args>(args)...);
+            new (elements.data() + chunk_size) T(std::forward<Args>(args)...);
             chunk_size++;
         }
-
 
         T & operator[](std::size_t index) {
             return elements[index];
@@ -60,6 +59,73 @@ class StableVector {
         std::array<T, ChunkSize> elements;
         // make sure T has default constructor
         std::size_t chunk_size = 0;
+    };
+
+    class Iterator {
+    public:
+        Iterator(StableVector<T, ChunkSize, InitialCapacity> & stable_vec, std::size_t index) : stable_vec_(stable_vec), index_(index) {}
+        Iterator(const Iterator & other) : stable_vec_(other.stable_vec_), index_(other.index_) {}
+        
+        T & operator*() {
+            return stable_vec_[index_];
+        }
+
+        const T & operator*() const {
+            return stable_vec_[index_];
+        }
+
+        T * operator->() {
+            return &stable_vec_[index_];
+        }
+
+        const T * operator->() const {
+            return &stable_vec_[index_];
+        }
+
+        Iterator & operator++() {
+            index_++;
+            return *this;
+        }
+
+        Iterator operator++(int) {
+            Iterator tmp = *this;
+            index_++;
+            return tmp;
+        }
+        
+        Iterator & operator--() {
+            index_--;
+            return *this;
+        }
+
+        Iterator operator--(int) {
+            Iterator tmp = *this;
+            index_--;
+            return tmp;
+        }
+
+        Iterator operator+(std::size_t n) const {
+            return Iterator(stable_vec_, index_ + n);
+        }
+
+        Iterator operator-(std::size_t n) const {
+            return Iterator(stable_vec_, index_ - n);
+        }
+
+        bool operator==(const Iterator & other) const {
+            if(index_ >= stable_vec_.size() && other.index_ >= stable_vec_.size()) {
+                // both is end iterator
+                return true;
+            }
+            return index_ == other.index_;
+        }
+
+        bool operator!=(const Iterator & other) const {
+            return !operator==(other);
+        }
+    private:
+        StableVector<T, ChunkSize, InitialCapacity> & stable_vec_;
+        std::size_t index_;
     };
 
 public:
@@ -80,26 +146,29 @@ public:
     }
 
     const T & at(std::size_t index) {
+        if(index >= size_) {
+            throw std::out_of_range("[StableVector::at] index out of range");
+        }
         return chunks_.at(index / ChunkSize) -> at(index % ChunkSize);
     }
 
     void push_back(const T & element) {
-        get_last_chunk().push_back(element);
+        get_last_chunk_for_insert().push_back(element);
         size_++;
     }
 
     template <typename ... Args>
     void emplace_back(Args && ... args) {
-        get_last_chunk().emplace_back(std::forward<Args>(args)...);
+        get_last_chunk_for_insert().emplace_back(std::forward<Args>(args)...);
         size_++;
     }
 
-    void resize(std::size_t capacity) {
-        if(capacity > size_) {
+    void expand_capacity_to(std::size_t capacity) {
+        if((capacity / ChunkSize) > (size_ / ChunkSize)) {
             // std::unique_ptr is not copyable, so we need to use resize
             // and then copy the elements
             chunks_.resize(capacity / ChunkSize);
-            for(size_t i = size_ / ChunkSize + 1; i <= capacity / ChunkSize; i++) {
+            for(size_t i = size_ / ChunkSize; i < capacity / ChunkSize; i++) {
                 chunks_[i] = std::make_unique<Chunk>();
             }
         }
@@ -113,13 +182,49 @@ public:
         return (size_ == 0);
     }
 
+    Iterator begin() {
+        return Iterator(*this, 0);
+    }
+
+    Iterator end() {
+        return Iterator(*this, size_);
+    }
+
+    T & front() {
+        if(size_ == 0) {
+            throw std::out_of_range("[StableVector::front]");
+        }
+        return (*(chunks_[0]))[0];
+    }
+
+    T & back() {
+        if(size_ == 0) {
+            throw std::out_of_range("[StableVector::back]");
+        }
+        return (*(chunks_[(size_ - 1) / ChunkSize]))[(size_ - 1) % ChunkSize];
+    }
+
+    void clear() {
+        chunks_.clear();
+        chunks_.reserve(InitialCapacity / ChunkSize);
+        for (size_t i = 0; i < InitialCapacity / ChunkSize; ++i) {
+            chunks_.push_back(std::make_unique<Chunk>());
+        }
+        size_ = 0;
+    }
+
 private:
-    Chunk & get_last_chunk() {
+    Chunk & get_last_chunk_for_insert() {
         // DEBUGING
         #ifdef DEBUG_STABLE_VECTOR
-        std::cout << "[get_last_chunk] size_: " << size_ << ", chunks_.size(): " << chunks_.size() << std::endl;
+        std::cout << "[get_last_chunk_for_insert] size_: " << size_ << ", chunks_.size(): " << chunks_.size() << std::endl;
         #endif
         // DEBUGING
+
+        if(((size_ % ChunkSize) == 0) && ((size_ / ChunkSize) == chunks_.size())) {
+            // no more space, need to insert a new chunk
+            chunks_.push_back(std::make_unique<Chunk>());
+        }
 
         return *(chunks_[size_ / ChunkSize]);
     }
--- stable_vector/stable_vector_test.cpp
+++ stable_vector/stable_vector_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cassert>
+#include <vector>
 #include "stable_vector.hpp"
 
 using namespace hpds;
@@ -29,133 +30,142 @@ void test_basic_operations() {
     all_passed = (vec.size() == 3);
     print_test_result("Push back", all_passed);
 
-    vec.resize(1408);
+    vec.expand_capacity_to(1024);
 
     // Test element access
     all_passed = (vec[0] == 1 && vec[1] == 2 && vec[2] == 3);
     print_test_result("Element access", all_passed);
     
-    // // Test front and back
-    // all_passed = (vec.front() == 1 && vec.back() == 3);
-    // print_test_result("Front and back", all_passed);
+    // Test front and back
+    all_passed = (vec.front() == 1 && vec.back() == 3);
+    print_test_result("Front and back", all_passed);
     
-    // // Test clear
-    // vec.clear();
-    // all_passed = (vec.empty() && vec.size() == 0);
-    // print_test_result("Clear", all_passed);
+    // Test clear
+    vec.clear();
+    all_passed = (vec.empty() && vec.size() == 0);
+    print_test_result("Clear", all_passed);
 }
 
-// // Test iterator stability
-// void test_iterator_stability() {
-//     std::cout << "\n=== Testing Iterator Stability ===" << std::endl;
+// Test iterator stability
+void test_iterator_stability() {
+    std::cout << "\n=== Testing Iterator Stability ===" << std::endl;
     
-//     StableVector<std::string> vec;
-//     bool all_passed = true;
+    StableVector<std::string> vec;
+    bool all_passed = true;
     
-//     // Insert elements
-//     vec.push_back("first");
-//     vec.push_back("second");
-//     vec.push_back("third");
-    
-//     // Get iterators
-//     auto it1 = vec.begin();
-//     auto it2 = vec.begin() + 1;
-//     auto it3 = vec.begin() + 2;
-    
-//     // Store values
-//     std::string val1 = *it1;
-//     std::string val2 = *it2;
-//     std::string val3 = *it3;
-    
-//     // Insert element in the middle
-//     vec.insert(vec.begin() + 1, "inserted");
-    
-//     // Verify iterators still point to the same elements
-//     all_passed &= (*it1 == val1);
-//     all_passed &= (*it2 == val2);
-//     all_passed &= (*it3 == val3);
-//     print_test_result("Iterator stability after insert", all_passed);
-// }
+    // Insert elements
+    vec.push_back("first");
+    vec.push_back("second");
+    vec.push_back("third");
 
-// // Test erase operations
-// void test_erase_operations() {
-//     std::cout << "\n=== Testing Erase Operations ===" << std::endl;
-    
-//     StableVector<int> vec;
-//     bool all_passed = true;
-    
-//     // Insert elements
-//     for (int i = 0; i < 5; ++i) {
-//         vec.push_back(i);
-//     }
-    
-//     // Get iterator to middle element
-//     auto it = vec.begin() + 2;
-//     int value = *it;
-    
-//     // Erase element
-//     vec.erase(vec.begin() + 1);
-    
-//     // Verify iterator still points to the same element
-//     all_passed &= (*it == value);
-//     all_passed &= (vec.size() == 4);
-//     print_test_result("Iterator stability after erase", all_passed);
-    
-//     // Verify other elements
-//     all_passed &= (vec[0] == 0);
-//     all_passed &= (vec[1] == 2);
-//     all_passed &= (vec[2] == 3);
-//     all_passed &= (vec[3] == 4);
-//     print_test_result("Element order after erase", all_passed);
-// }
+    auto it = vec.begin();
+    vec.emplace_back("fourth");
 
-// // Test with custom type
-// struct TestStruct {
-//     int value;
-//     std::string name;
-    
-//     TestStruct(int v, const std::string& n) : value(v), name(n) {}
-    
-//     bool operator==(const TestStruct& other) const {
-//         return value == other.value && name == other.name;
-//     }
-// };
+    all_passed &= (*it == "first");
+    all_passed &= (*(it + 1) == "second");
+    all_passed &= (*(it + 2) == "third");
+    all_passed &= (*(it + 3) == "fourth");
+    print_test_result("Iterator stability", all_passed);
+}
 
-// void test_custom_type() {
-//     std::cout << "\n=== Testing Custom Type ===" << std::endl;
+// Test with custom type
+struct TestStruct {
+    int value;
+    std::string name;
+    TestStruct() : value(0), name("") {}
+
+    TestStruct(int v, const std::string& n) : value(v), name(n) {}
     
-//     StableVector<TestStruct> vec;
-//     bool all_passed = true;
+    bool operator==(const TestStruct& other) const {
+        return value == other.value && name == other.name;
+    }
+};
+
+void test_custom_type() {
+    std::cout << "\n=== Testing Custom Type ===" << std::endl;
     
-//     // Insert elements
-//     vec.push_back(TestStruct(1, "one"));
-//     vec.push_back(TestStruct(2, "two"));
+    StableVector<TestStruct> vec;
+    bool all_passed = true;
     
-//     // Get iterator
-//     auto it = vec.begin();
+    // Insert new element
+    vec.emplace_back(0, "zero");
+
+    // Get iterator
+    auto it = vec.begin();
     
-//     // Insert new element
-//     vec.insert(vec.begin(), TestStruct(0, "zero"));
+    // Insert elements
+    vec.push_back(TestStruct(1, "one"));
+    vec.push_back(TestStruct(2, "two"));
+
+    // Verify iterator stability
+    all_passed &= (*it == TestStruct(0, "zero"));
+    all_passed &= (*(it + 1) == TestStruct(1, "one"));
+    all_passed &= (*(it + 2) == TestStruct(2, "two"));
+    print_test_result("Custom type iterator stability", all_passed);
     
-//     // Verify iterator stability
-//     all_passed &= (it->value == 1);
-//     all_passed &= (it->name == "one");
-//     print_test_result("Custom type iterator stability", all_passed);
+    // Verify all elements
+    all_passed &= (vec[0].value == 0 && vec[0].name == "zero");
+    all_passed &= (vec[1].value == 1 && vec[1].name == "one");
+    all_passed &= (vec[2].value == 2 && vec[2].name == "two");
+    print_test_result("Custom type element access", all_passed);
+}
+
+// Comparison test with std::vector
+void test_comparison_with_std_vector() {
+    std::cout << "\n=== Testing Comparison with std::vector ===" << std::endl;
     
-//     // Verify all elements
-//     all_passed &= (vec[0].value == 0 && vec[0].name == "zero");
-//     all_passed &= (vec[1].value == 1 && vec[1].name == "one");
-//     all_passed &= (vec[2].value == 2 && vec[2].name == "two");
-//     print_test_result("Custom type element access", all_passed);
-// }
+    StableVector<int> stable_vec;
+    std::vector<int> std_vec;
+    bool all_passed = true;
+    
+    // Test push_back operations
+    for (int i = 0; i < 1000; ++i) {
+        stable_vec.push_back(i);
+        std_vec.push_back(i);
+        all_passed &= (stable_vec.size() == std_vec.size());
+        all_passed &= (stable_vec.back() == std_vec.back());
+    }
+    print_test_result("Push back comparison", all_passed);
+    
+    // Test iterator operations
+    all_passed = true;
+    auto stable_it = stable_vec.begin();
+    auto std_it = std_vec.begin();
+    
+    for (size_t i = 0; i < 1000; ++i) {
+        all_passed &= (*stable_it == *std_it);
+        ++stable_it;
+        ++std_it;
+    }
+    print_test_result("Iterator comparison", all_passed);
+    
+    // Test random access
+    all_passed = true;
+    for (size_t i = 0; i < 1000; ++i) {
+        all_passed &= (stable_vec[i] == std_vec[i]);
+    }
+    print_test_result("Random access comparison", all_passed);
+    
+    // Test front and back
+    all_passed = (stable_vec.front() == std_vec.front() && 
+                 stable_vec.back() == std_vec.back());
+    print_test_result("Front and back comparison", all_passed);
+    
+    // Test clear
+    stable_vec.clear();
+    std_vec.clear();
+    all_passed = (stable_vec.empty() == std_vec.empty() && 
+                 stable_vec.size() == std_vec.size());
+    print_test_result("Clear comparison", all_passed);
+}
 
 int main() {
     std::cout << "Starting StableVector Tests...\n" << std::endl;
     
     test_basic_operations();
-    // test_iterator_stability();
-    // test_erase_operations();
-    // test_custom_type();
+    test_iterator_stability();
+    test_custom_type();
+    test_comparison_with_std_vector();
     
     std::cout << "\nAll tests completed!" << std::endl;
     return 0;
